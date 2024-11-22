bbc9ae3ecc00f3852be05c6e0757dbcddaa55730
Code Chooch <codechooch@gmail.com>
2024-11-22 10:17:48 -0800

    Init nuqs

---
+++ src/components/providers.tsx
@@ -0,0 +1,6 @@
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
+import { ReactNode } from 'react'
+
+export function Providers({ children }: { children: ReactNode }) {
+  return <NuqsAdapter>{children}</NuqsAdapter>
+}
