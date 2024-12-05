63527e29fa71c44ee6bf34ea68b87855597a3a6b
Code Chooch <codechooch@gmail.com>
2024-12-04 16:59:00 -0800

    add redirects for admin pages

--- src/app/admin/affiliates/page.tsx
+++ src/app/admin/affiliates/page.tsx
@@ -1,3 +1,9 @@
-export default function AdminAffiliatesPage() {
+import { isUserAdmin } from '@/lib/clerk'
+import { redirect } from 'next/navigation'
+
+export default function Page() {
+  if (!isUserAdmin) {
+    redirect('/')
+  }
   return <h1>Admin Affiliates Page</h1>
 }
--- src/app/admin/notify/page.tsx
+++ src/app/admin/notify/page.tsx
@@ -3,24 +3,44 @@ import { notifyTable } from '@/db/schema'
 import { NotifyUserStats } from '@/features/soon/components/notify-user-stats'
 import { NotifyUserType } from '@/features/soon/types'
 import { Suspense } from 'react'
+import { isUserAdmin } from '@/lib/clerk'
+import { redirect } from 'next/navigation'
 
 export default async function Page() {
+  if (!isUserAdmin) {
+    redirect('/')
+  }
+
   const userData = await db
-    .select({ email: notifyTable.email, userType: notifyTable.userType })
+    .select({
+      email: notifyTable.email,
+      userType: notifyTable.userType,
+      notifiedAt: notifyTable.notifiedAt,
+    })
     .from(notifyTable)
 
   const stats = {
-    employers: userData.filter(
-      (u) => (u.userType as NotifyUserType) === 'employer'
+    employersNotNotified: userData.filter(
+      (u) =>
+        (u.userType as NotifyUserType) === 'employer' && u.notifiedAt === null
     ).length,
-    developers: userData.filter(
-      (u) => (u.userType as NotifyUserType) === 'developer'
+    employersNotified: userData.filter(
+      (u) =>
+        (u.userType as NotifyUserType) === 'employer' && u.notifiedAt !== null
+    ).length,
+    developersNotNotified: userData.filter(
+      (u) =>
+        (u.userType as NotifyUserType) === 'developer' && u.notifiedAt === null
+    ).length,
+    developersNotified: userData.filter(
+      (u) =>
+        (u.userType as NotifyUserType) === 'developer' && u.notifiedAt !== null
     ).length,
   }
 
   return (
     <div className="container mx-auto py-10">
-      <h1 className="text-2xl font-bold mb-5">Go Live Notification Requests</h1>
+      <h1 className="text-2xl font-bold mb-5">Go Live Notifications Sent</h1>
       <Suspense fallback={<div>Loading...</div>}>
         <NotifyUserStats stats={stats} />
       </Suspense>
